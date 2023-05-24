cc7f121dd4e8cd56ea9ef944f3766686296073a3
Hernan Martinez <hernanmartinezf@gmail.com>
2023-05-23 21:37:17 -0300

    ensamblando navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Index from './pages/index';
 import LogIn from './pages/login';
+import Menu from './components/menu';
 
 function App() {
   return (
     <div className='App'>  
+    <Menu />
     <LogIn />
       {/* <Index /> */}
     </div>
