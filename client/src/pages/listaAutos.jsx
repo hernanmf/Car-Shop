3010ae01fd370b0e6210ff6b0a2f4a299fdd2f6e
Hernan Martinez <hernanmartinezf@gmail.com>
2023-06-06 19:44:12 -0300

    Listado de autos

--- client/src/pages/listaAutos.jsx
+++ client/src/pages/listaAutos.jsx
@@ -5,26 +5,42 @@ import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
 
 import '../css/bloques.css';
 
 const ListaAutos = () => {
   return (
     <>
-      <Container>
-        <div className='bloques-cerrado'>
-          <div className='cards-container'>
+      <div className='bloques-cerrado'>
+        <Container>
+          <h5>AUTOS</h5>
+          <br />
 
-          <Row xs={1} md={3} /* className="g-3" */>
+          <Row xs={1} md={4}>
           
-            <Col /* key={} */>
-            <Card style={{ width: '18rem' }}>
-            <img
+            <Col /* key={} */className='mb-5' >
+            <Card style={{ width: '18rem', height: '24rem'}} >
+            <Image
               alt=""
               src="http://www.motoresapleno.com.ar/wp-content/uploads/2014/04/Ford-Fiesta-Kinetic-Design-Trend-Plus-1.6-Sedan-1.jpg"
-              width='100%'
-              height="100%"
-              className="d-inline-block"
+              fluid
+            />
+            <Card.Body>
+              <h6>Ford Fiesta Kinetic Design Titanium</h6>
+              <h5>$ 3.400.000</h5>
+              <p className="text-muted">2017 - 70000 km</p>
+              <Button variant="danger">Ver mas</Button>
+            </Card.Body>
+            </Card>
+            </Col>
+              
+            <Col /* key={} */ className='mb-5'>
+            <Card style={{ width: '18rem', height: '24rem' }} >
+            <Image
+              alt=""
+              src="https://resizer.glanacion.com/resizer/dWlOngl_skg1doCItj44-FwuyMI=/1200x800/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/VOC5FV2Y2NFZXL7MVI75I3IJWI.jpg"
+              fluid    
             />
             <Card.Body>
               <h6>Ford Fiesta Kinetic Design Titanium</h6>
@@ -35,14 +51,12 @@ const ListaAutos = () => {
             </Card>
               </Col>
               
-            <Col /* key={} */>
-            <Card style={{ width: '18rem' }}>
-            <img
+            <Col /* key={} */ className='mb-5'>
+            <Card style={{ width: '18rem', height: '24rem'}}>
+            <Image
               alt=""
-              src="http://www.motoresapleno.com.ar/wp-content/uploads/2014/04/Ford-Fiesta-Kinetic-Design-Trend-Plus-1.6-Sedan-1.jpg"
-              width='100%'
-              height="100%"
-              className="d-inline-block"
+              src="https://img.remediosdigitales.com/404f41/citroen_c4_cactus_10/1366_2000.jpg"
+              fluid
             />
             <Card.Body>
               <h6>Ford Fiesta Kinetic Design Titanium</h6>
@@ -51,16 +65,46 @@ const ListaAutos = () => {
               <Button variant="danger">Ver mas</Button>
             </Card.Body>
             </Card>
-              </Col>
-              
-            <Col /* key={} */>
-            <Card style={{ width: '18rem' }}>
-            <img
+            </Col>
+            
+            <Col /* key={} */ className='mb-5'>
+            <Card style={{ width: '18rem',height: '24rem' }} >
+            <Image
               alt=""
               src="http://www.motoresapleno.com.ar/wp-content/uploads/2014/04/Ford-Fiesta-Kinetic-Design-Trend-Plus-1.6-Sedan-1.jpg"
-              width='100%'
-              height="100%"
-              className="d-inline-block"
+              fluid
+            />
+            <Card.Body>
+              <h6>Ford Fiesta Kinetic Design Titanium</h6>
+              <h5>$ 3.400.000</h5>
+              <p className="text-muted">2017 - 70000 km</p>
+              <Button variant="danger">Ver mas</Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            
+            <Col /* key={} */ className='mb-5'>
+            <Card style={{ width: '18rem', height: '24rem' }} >
+            <Image
+              alt=""
+              src="http://www.motoresapleno.com.ar/wp-content/uploads/2014/04/Ford-Fiesta-Kinetic-Design-Trend-Plus-1.6-Sedan-1.jpg"
+              fluid    
+            />
+            <Card.Body>
+              <h6>Ford Fiesta Kinetic Design Titanium</h6>
+              <h5>$ 3.400.000</h5>
+              <p className="text-muted">2017 - 70000 km</p>
+              <Button variant="danger">Ver mas</Button>
+            </Card.Body>
+            </Card>
+            </Col>
+
+            <Col /* key={} */ className='mb-5'>
+            <Card style={{ width: '18rem',height: '24rem' }} >
+            <Image
+              alt=""
+              src="https://img.remediosdigitales.com/404f41/citroen_c4_cactus_10/1366_2000.jpg"
+              fluid 
             />
             <Card.Body>
               <h6>Ford Fiesta Kinetic Design Titanium</h6>
@@ -72,9 +116,8 @@ const ListaAutos = () => {
             </Col>
           
             </Row>
-          </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </>
   );
 }
