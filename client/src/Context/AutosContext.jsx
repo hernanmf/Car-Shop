09086a707bf251bd547fe309135f00364d636e47
Hernan Martinez <hernanmartinezf@gmail.com>
2023-06-24 20:28:34 -0300

    intentando que funcione active car

--- client/src/Context/AutosContext.jsx
+++ client/src/Context/AutosContext.jsx
@@ -1,12 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-let autos = require('../datos/autos'); //importacion de los datos desde otro script
+let scriptAutos = require('../datos/autos'); //importacion de los datos desde otro script
 
 export const AutosContext = createContext();
 
 export const AutosProvider = ({ children }) => {
+
+  const [activeCar, setactiveCar] = useState(false);
+  const [ autos ] = useState(scriptAutos);
+
   return (
-    <AutosContext.Provider value={ autos }>
+    <AutosContext.Provider value={{ autos, activeCar, setactiveCar }}>
       { children }
     </AutosContext.Provider>
   )
--- client/src/pages/misPublicaciones.jsx
+++ client/src/pages/misPublicaciones.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { AutosContext } from '../Context/AutosContext';
+import { UsuariosContext } from '../Context/UserContext';
+import { Link } from 'react-router-dom';
 
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/esm/Container';
@@ -8,13 +10,56 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
 
 import '../css/bloques.css';
-import { Link } from 'react-router-dom';
 
 const MisPublicaciones = () => {
+  
+  const { activeUser } = useContext(UsuariosContext);
+  const { autos, activeCar, setactiveCar } = useContext(AutosContext);
+  let mispublicaciones = autos.filter(auto => auto.idusuario === activeUser.id);
+  
+  const handleActiveCar = (idAuto) => {
+    /* const form = e.currentTarget; */
 
-  const autos = useContext( AutosContext );
+    console.log(idAuto);
+    alert();
+
+   /*  console.log('lo que me trae el value del boton: ',form.btnVer.value );
+    alert('lo que me trae el value del boton: ', form.btnVer.value);
+
+    let newactiveCar = mispublicaciones.find((auto => auto.id === form.btnVer.value));
+    console.log('lo que me trae el arreglo con find: ',newactiveCar);
+    alert('lo que me trae el arreglo con find: ', newactiveCar);
+    
+    setactiveCar(newactiveCar);
+    console.log('Auto activo: ',activeCar);
+    alert('Auto activo: ', activeCar);
+    
+    console.log('Auto activo: ', activeCar);
+    alert('Auto activo: ', activeCar);
+    e.preventDefault();
+    e.stopPropagation();
+    
+
+    console.log('lo que me trae el value del boton: ',form.btnVer.value );
+    alert('lo que me trae el value del boton: ', form.btnVer.value);
+
+    let newactiveCar = mispublicaciones.find((auto => auto.id === form.btnVer.value));
+    console.log('lo que me trae el arreglo con find: ',newactiveCar);
+    alert('lo que me trae el arreglo con find: ', newactiveCar);
+    
+    setactiveCar(newactiveCar);
+    console.log('Auto activo: ',activeCar);
+    alert('Auto activo: ', activeCar);
+    
+    console.log('Auto activo: ', activeCar);
+    alert('Auto activo: ', activeCar); 
+    e.preventDefault();
+    e.stopPropagation();
+    */
+  };
 
   return (
     <>
@@ -26,34 +71,36 @@ const MisPublicaciones = () => {
           <br />
 
           <Row xs={1} md={4} className='justify-content-center'>
-            {autos.map((auto) => (
-              <Col className='mb-5' >
-              <Card style={{ maxWidth: '100%', maxHeight:'52vh'}} >
-                <Image alt="" src={auto.fotos[0]} fluid/>
-                <Card.Body>
-                  <h6>{auto.marca} {auto.modelo} {auto.version}</h6>
-                    <h5>$ {auto.precio}</h5>
-                  <p className="text-muted">{auto.anio} - {auto.kilometros}km</p>
-                    <ButtonGroup size="sm">
-                      <Button variant="danger">
-                        <Link to="/vistavehiculo" style={{ textDecoration: 'none' }}>
-                        Ver
-                        </Link>
-                      </Button>
-                      <Button variant="danger">
-                        <Link to="/editarauto" style={{ textDecoration: 'none' }}>
-                          Editar
-                        </Link>
-                      </Button>
-                      <Button variant="danger">
-                        <Link to="/misdatos" style={{ textDecoration: 'none' }}>
-                          Eliminar
-                        </Link>
-                      </Button>
-                    </ButtonGroup>
-                  </Card.Body>
-                </Card>
-              </Col>))
+            {mispublicaciones.map((auto) => (
+                <Col className='mb-5' >
+                  <Card style={{ maxWidth: '100%', maxHeight: '52vh' }} >
+                    <Image alt="" src={auto.fotos[0]} fluid />
+                    <Card.Body>
+                      <h6>{auto.marca} {auto.modelo} {auto.version}</h6>
+                      <h5>$ {auto.precio}</h5>
+                      <p className="text-muted">{auto.anio} - {auto.kilometros}km</p>
+                      {/* <Form onSubmit={handleActiveCar}> */}
+                      <ButtonGroup size="sm">
+                      <Button variant="danger" type='submit' id='btnVer' onClick={handleActiveCar(auto.id)} /* value={`${auto.id}`} */ /* onClick={handleActiveCar} */>
+                          <Link to="/vistavehiculo" style={{ textDecoration: 'none' }}>
+                          Ver
+                          </Link>
+                        </Button>
+                      <Button variant="danger" type='submit' id='btnEditar' value={`${auto.id}`} /* onClick={handleActiveCar} */>
+                          <Link to="/editarauto" style={{ textDecoration: 'none' }} >
+                            Editar
+                          </Link>
+                        </Button>
+                        <Button variant="danger" type='submit' id='btnEliminar' value={`${auto.id}`}>
+                          <Link to="/misdatos" style={{ textDecoration: 'none' }}>
+                            Eliminar
+                          </Link>
+                        </Button>
+                      </ButtonGroup>
+                      {/* </Form> */}
+                    </Card.Body>
+                  </Card>
+                </Col>))
             }
  
           </Row>
