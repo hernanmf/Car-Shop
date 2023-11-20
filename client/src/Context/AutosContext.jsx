a14e6e85ebd35d9700ea8157a99dfe2e00c9b47b
hernanmf <hernanmartinezf@gmail.com>
2023-11-20 15:10:08 -0300

    limpieza de los context

--- client/src/Context/AutosContext.jsx
+++ client/src/Context/AutosContext.jsx
@@ -1,4 +1,3 @@
-/*  VERSION MIA QUE AL REFRESCAR SE PIERDEN LOS DATOS NUEVOS DEL CONTEXTO */
 import { createContext, useState } from "react";
 
 let scriptAutos = require('../datos/autos'); //importacion de los datos desde otro script
@@ -17,37 +16,3 @@ export const AutosProvider = ({ children }) => {
     </AutosContext.Provider>
   )
 };
-
-/*  VERSION DE CHATGPT QUE AL REFRESCAR SI PERSISTEN LOS DATOS NUEVOS DEL CONTEXTO -- pero que no funciona
-import { createContext, useState, useEffect } from "react";
-
-export const AutosContext = createContext();
-
-export const AutosProvider = ({ children }) => {
-  const [activeCar, setactiveCar] = useState(false);
-  const [autos, setAutos] = useState([]);
-  const [listado, setListado] = useState([]);
-
-  // Cargar los datos de autos del almacenamiento local cuando se cargue la página
-  useEffect(() => {
-    const storedAutos = localStorage.getItem('autos');
-    if (storedAutos) {
-      setAutos(JSON.parse(storedAutos));
-      setListado(JSON.parse(storedAutos));
-    } else {
-      setAutos(require('../datos/autos'));
-      setListado(require('../datos/autos'));
-    }
-  }, []);
-
-  // Guardar los datos de autos en el almacenamiento local cada vez que se actualicen
-  useEffect(() => {
-    localStorage.setItem('autos', JSON.stringify(autos));
-  }, [autos]);
-
-  return (
-    <AutosContext.Provider value={{ autos, setAutos, activeCar, setactiveCar, listado, setListado }}>
-      {children}
-    </AutosContext.Provider>
-  );
-};*/
--- client/src/Context/UserContext.jsx
+++ client/src/Context/UserContext.jsx
@@ -1,4 +1,3 @@
-/*  VERSION MIA QUE AL REFRESCAR SE PIERDEN LOS DATOS NUEVOS DEL CONTEXTO */
 import { createContext, useState } from "react";
 
 let scriptUsuarios = require('../datos/usuarios'); //importacion de los datos desde otro script
@@ -16,36 +15,3 @@ export const UsuariosProvider = ({ children }) => {
     </UsuariosContext.Provider>
   )
 };
-
-/*  VERSION DE CHATGPT QUE AL REFRESCAR SI PERSISTEN LOS DATOS NUEVOS DEL CONTEXTO--- pero que no funciona
-import { createContext, useState, useEffect } from "react";
-
-export const UsuariosContext = createContext();
-
-export const UsuariosProvider = ({ children }) => {
-  
-  const [activeUser, setactiveUser] = useState(false);
-  const [usuarios, setUsuarios] = useState([]);
-  
-  // Cargar los datos de usuarios del almacenamiento local cuando se cargue la página
-  useEffect(() => {
-    const storedUsuarios = localStorage.getItem('usuarios');
-    if (storedUsuarios) {
-      setUsuarios(JSON.parse(storedUsuarios));
-    } else {
-      setUsuarios(require('../datos/usuarios'));
-    }
-  }, []);
-  
-  // Guardar los datos de usuarios en el almacenamiento local cada vez que se actualicen
-  useEffect(() => {
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
-  }, [usuarios]);
-  
-  return (
-    <UsuariosContext.Provider value={{ usuarios, setUsuarios, activeUser, setactiveUser }}>
-      { children }
-    </UsuariosContext.Provider>
-  )
-};
-*/
